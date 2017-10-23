53f931917322c90a89c767cb002cde09b27f181f
zhangze <zhangze@wanbu.com.cn>
2017-10-23 17:41:09 +0800

    项目初始化

---
+++ ZZeActionC/ZZeActionC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZeActionC
+//
+//  Created by yaoln on 2017/10/23.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZeActionC/ZZeActionC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZeActionC
+//
+//  Created by yaoln on 2017/10/23.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
