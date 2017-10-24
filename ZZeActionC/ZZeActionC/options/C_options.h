a5f840835aa178abe34fdab5901789bb1b5eb04b
zhangze <zhangze@wanbu.com.cn>
2017-10-24 17:50:41 +0800

    0.0.2 版本修改

---
+++ ZZeActionC/ZZeActionC/options/C_options.h
@@ -0,0 +1,17 @@
+//
+//  C_options.h
+//  ZZeActionC
+//
+//  Created by yaoln on 2017/10/24.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface C_options : NSObject
+/**
+ 调试模块的方法
+ */
+- (void)doSomething;
+
+@end
---
+++ ZZeActionC/ZZeActionC/options/C_options.m
@@ -0,0 +1,17 @@
+//
+//  C_options.m
+//  ZZeActionC
+//
+//  Created by yaoln on 2017/10/24.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import "C_options.h"
+
+@implementation C_options
+- (void)doSomething {
+    NSLog(@"ZZeActionC -- 0.0.1");
+    NSLog(@"ZZeActionC — 0.0.2");
+}
+
+@end
