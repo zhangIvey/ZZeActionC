00f9625f91b6596b0c33494401df1bbb46a2e588
zhangze <zhangze@wanbu.com.cn>
2017-10-25 20:57:01 +0800

    0.0.25

--- ZZeActionC/ZZeActionC/options/C_options.m
+++ ZZeActionC/ZZeActionC/options/C_options.m
@@ -27,6 +27,7 @@
     NSLog(@"ZZeActionC -- 0.0.19 ：添加第三方的依赖库:JSONKit,AFNetworking");
     NSLog(@"ZZeActionC -- 0.0.21 ：添加第三方的依赖库:JSONKit,AFNetworking");
     NSLog(@"ZZeActionC -- 0.0.24 ：添加第三方的依赖库:JSONKit,AFNetworking");
+    NSLog(@"ZZeActionC -- 0.0.25 ：添加第三方的依赖库:JSONKit,AFNetworking");
     D_options *d = [D_options new];
     [d doSomething];
 }
